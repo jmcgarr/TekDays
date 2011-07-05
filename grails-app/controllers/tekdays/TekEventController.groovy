b28764ddeab73d101a73ca34acd4e5a5e739cd2c
porterhouse91 <jmcgarr@gmail.com>
2011-07-05 07:08:13 -0400

    Ran the grails generate-all command to generate the controllers and views

--- grails-app/controllers/tekdays/TekEventController.groovy
+++ grails-app/controllers/tekdays/TekEventController.groovy
@@ -2,5 +2,99 @@ package tekdays
 
 class TekEventController {
 
-    def scaffold = TekEvent
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index = {
+        redirect(action: "list", params: params)
+    }
+
+    def list = {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+        [tekEventInstanceList: TekEvent.list(params), tekEventInstanceTotal: TekEvent.count()]
+    }
+
+    def create = {
+        def tekEventInstance = new TekEvent()
+        tekEventInstance.properties = params
+        return [tekEventInstance: tekEventInstance]
+    }
+
+    def save = {
+        def tekEventInstance = new TekEvent(params)
+        if (tekEventInstance.save(flush: true)) {
+            flash.message = "${message(code: 'default.created.message', args: [message(code: 'tekEvent.label', default: 'TekEvent'), tekEventInstance.id])}"
+            redirect(action: "show", id: tekEventInstance.id)
+        }
+        else {
+            render(view: "create", model: [tekEventInstance: tekEventInstance])
+        }
+    }
+
+    def show = {
+        def tekEventInstance = TekEvent.get(params.id)
+        if (!tekEventInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'tekEvent.label', default: 'TekEvent'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            [tekEventInstance: tekEventInstance]
+        }
+    }
+
+    def edit = {
+        def tekEventInstance = TekEvent.get(params.id)
+        if (!tekEventInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'tekEvent.label', default: 'TekEvent'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            return [tekEventInstance: tekEventInstance]
+        }
+    }
+
+    def update = {
+        def tekEventInstance = TekEvent.get(params.id)
+        if (tekEventInstance) {
+            if (params.version) {
+                def version = params.version.toLong()
+                if (tekEventInstance.version > version) {
+                    
+                    tekEventInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'tekEvent.label', default: 'TekEvent')] as Object[], "Another user has updated this TekEvent while you were editing")
+                    render(view: "edit", model: [tekEventInstance: tekEventInstance])
+                    return
+                }
+            }
+            tekEventInstance.properties = params
+            if (!tekEventInstance.hasErrors() && tekEventInstance.save(flush: true)) {
+                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'tekEvent.label', default: 'TekEvent'), tekEventInstance.id])}"
+                redirect(action: "show", id: tekEventInstance.id)
+            }
+            else {
+                render(view: "edit", model: [tekEventInstance: tekEventInstance])
+            }
+        }
+        else {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'tekEvent.label', default: 'TekEvent'), params.id])}"
+            redirect(action: "list")
+        }
+    }
+
+    def delete = {
+        def tekEventInstance = TekEvent.get(params.id)
+        if (tekEventInstance) {
+            try {
+                tekEventInstance.delete(flush: true)
+                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'tekEvent.label', default: 'TekEvent'), params.id])}"
+                redirect(action: "list")
+            }
+            catch (org.springframework.dao.DataIntegrityViolationException e) {
+                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'tekEvent.label', default: 'TekEvent'), params.id])}"
+                redirect(action: "show", id: params.id)
+            }
+        }
+        else {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'tekEvent.label', default: 'TekEvent'), params.id])}"
+            redirect(action: "list")
+        }
+    }
 }
--- grails-app/controllers/tekdays/TekUserController.groovy
+++ grails-app/controllers/tekdays/TekUserController.groovy
@@ -2,5 +2,99 @@ package tekdays
 
 class TekUserController {
 
-    def scaffold = TekUser
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index = {
+        redirect(action: "list", params: params)
+    }
+
+    def list = {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+        [tekUserInstanceList: TekUser.list(params), tekUserInstanceTotal: TekUser.count()]
+    }
+
+    def create = {
+        def tekUserInstance = new TekUser()
+        tekUserInstance.properties = params
+        return [tekUserInstance: tekUserInstance]
+    }
+
+    def save = {
+        def tekUserInstance = new TekUser(params)
+        if (tekUserInstance.save(flush: true)) {
+            flash.message = "${message(code: 'default.created.message', args: [message(code: 'tekUser.label', default: 'TekUser'), tekUserInstance.id])}"
+            redirect(action: "show", id: tekUserInstance.id)
+        }
+        else {
+            render(view: "create", model: [tekUserInstance: tekUserInstance])
+        }
+    }
+
+    def show = {
+        def tekUserInstance = TekUser.get(params.id)
+        if (!tekUserInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'tekUser.label', default: 'TekUser'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            [tekUserInstance: tekUserInstance]
+        }
+    }
+
+    def edit = {
+        def tekUserInstance = TekUser.get(params.id)
+        if (!tekUserInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'tekUser.label', default: 'TekUser'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            return [tekUserInstance: tekUserInstance]
+        }
+    }
+
+    def update = {
+        def tekUserInstance = TekUser.get(params.id)
+        if (tekUserInstance) {
+            if (params.version) {
+                def version = params.version.toLong()
+                if (tekUserInstance.version > version) {
+                    
+                    tekUserInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'tekUser.label', default: 'TekUser')] as Object[], "Another user has updated this TekUser while you were editing")
+                    render(view: "edit", model: [tekUserInstance: tekUserInstance])
+                    return
+                }
+            }
+            tekUserInstance.properties = params
+            if (!tekUserInstance.hasErrors() && tekUserInstance.save(flush: true)) {
+                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'tekUser.label', default: 'TekUser'), tekUserInstance.id])}"
+                redirect(action: "show", id: tekUserInstance.id)
+            }
+            else {
+                render(view: "edit", model: [tekUserInstance: tekUserInstance])
+            }
+        }
+        else {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'tekUser.label', default: 'TekUser'), params.id])}"
+            redirect(action: "list")
+        }
+    }
+
+    def delete = {
+        def tekUserInstance = TekUser.get(params.id)
+        if (tekUserInstance) {
+            try {
+                tekUserInstance.delete(flush: true)
+                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'tekUser.label', default: 'TekUser'), params.id])}"
+                redirect(action: "list")
+            }
+            catch (org.springframework.dao.DataIntegrityViolationException e) {
+                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'tekUser.label', default: 'TekUser'), params.id])}"
+                redirect(action: "show", id: params.id)
+            }
+        }
+        else {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'tekUser.label', default: 'TekUser'), params.id])}"
+            redirect(action: "list")
+        }
+    }
 }
